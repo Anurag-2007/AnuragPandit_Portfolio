9c7d08d6423ac4a805b0af2b94da04786befa61e
Anurag-2007 <panditanurag3101@gmail.com>
2026-09-07 05:07:03 +0530

    fix snap 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoadingScreen from './components/LoadingScreen';
 import Index from './components/Index';
 import Photography from './components/Photography';
